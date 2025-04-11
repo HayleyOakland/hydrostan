6823989a77d2c4bf9ef4cd3c6e60df7ae724a7fa
gcpoole <gpoole@eco-metrics.com>
2025-04-11 14:01:27 -0600

    Update dedensify to use an offset argument to determine how crowded x values are toward the left.

--- R/utils.R
+++ R/utils.R
@@ -3,30 +3,28 @@
 #' Sample data using evenly spaced orders of magnitude.
 #'
 #' @param x vector of data values in x dimension or a data.frame
-#' @param x_col,ycol name or number of data.frame column containing x dimension and y
-#'   dimension
 #' @param y vector of data values in y dimension (equal in length to x)
 #' @param n number of samples to extract
+#' @param x_offset value added to x before log10 is applied; large values
+#'   provide more evenly spaced points.  Small values crowd points toward the
+#'   left (smaller values of x).
+#' @param x_col,ycol name or number of data.frame column containing x dimension
+#'   and y dimension
+#' @param ... other values passed on to dedensify.
 #' @return a tibble of n {x,y} pairs
 #' @export
 #' @rdname dedensify
 #' @export
-dedensify <- function(x, y, n) {
+dedensify <- function(x, y, n, x_offset = 1, decimals = 10) {
+
   y = y[order(x)]
   x = sort(x)
 
-  n_zeros <- sum(x==0)
-  if(sum(x<0) > 0 | n_zeros > 1) stop("Only one zero value and no negative values are allowed in x.")
+  x_off = x + x_offset
 
-  if(n_zeros) x = x[-1]
-  log_x = log10(x)
+  log_x = log10(x_off)
   log_deden_x = seq(min(log_x), max(log_x), length.out = n)
-  deden_x = 10^log_deden_x
-
-  if(n_zeros) {
-    x <- c(0, x)
-    deden_x <- c(0, deden_x)
-  }
+  deden_x = round(10^log_deden_x - x_offset, decimals)
 
   approx(x, y, deden_x) |>
     dplyr::as_tibble()
@@ -34,7 +32,7 @@ dedensify <- function(x, y, n) {
 
 #' @rdname dedensify
 #' @export
-dedensify_df <- function(x, x_col, y_col, n) {
-  dedensify(x[[x_col]], x[[y_col]], n = n)
+dedensify_df <- function(x, x_col, y_col, n, ...) {
+  dedensify(x[[x_col]], x[[y_col]], n, ...)
 }
 
